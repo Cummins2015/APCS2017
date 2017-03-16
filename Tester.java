10f08ec69d4996616df61c78c30e2f3b2c3f8211
Cummins2015 <mrpatrickcummins@gmail.com>
2017-03-16 08:29:25 -0400

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,16 @@
+public class Tester
+{
+   public static void main(String[] args)
+   {  
+        
+     // 1 //    Cow c = new Cow();
+     // 1 //   System.out.println(c.getType() + " goes " + c.getSound());
+   
+      Farm myFarm = new Farm();  ////create Farm object and run sound method on Farm ArrayList
+      myFarm.animalSounds();
+         
+   
+         
+       
+   }
+}
